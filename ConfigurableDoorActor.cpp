653c17d7fd3b33d229f3ce098c25f2e22d76dbc1
1250498077 <asd912345qq.com@qq.com>
2026-06-12 22:18:42 +0800

    asdasd

---
+++ ConfigurableDoorActor.cpp
@@ -0,0 +1,214 @@
+#include "ConfigurableDoorActor.h"
+
+#include "Components/SceneComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Curves/CurveFloat.h"
+
+AConfigurableDoorActor::AConfigurableDoorActor()
+{
+    PrimaryActorTick.bCanEverTick = true;
+
+    SceneRootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRootComponent"));
+    RootComponent = SceneRootComponent;
+
+    StaticMeshesRootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("StaticMeshesRootComponent"));
+    StaticMeshesRootComponent->SetupAttachment(SceneRootComponent);
+
+    DoorMotionRootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("DoorMotionRootComponent"));
+    DoorMotionRootComponent->SetupAttachment(SceneRootComponent);
+    DoorMotionRootComponent->SetMobility(EComponentMobility::Movable);
+
+    DoorMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DoorMeshComponent"));
+    DoorMeshComponent->SetupAttachment(DoorMotionRootComponent);
+    DoorMeshComponent->SetMobility(EComponentMobility::Movable);
+    DoorMeshComponent->SetCanEverAffectNavigation(false);
+}
+
+void AConfigurableDoorActor::BeginPlay()
+{
+    Super::BeginPlay();
+
+    bIsAnimating = false;
+    bIsOpen = bStartOpened;
+    ApplyDoorStateInstant(bStartOpened);
+}
+
+void AConfigurableDoorActor::OnConstruction(const FTransform& Transform)
+{
+    Super::OnConstruction(Transform);
+
+    if (GetWorld() && GetWorld()->IsGameWorld())
+    {
+        ApplyDoorStateInstant(bStartOpened);
+    }
+
+}
+
+void AConfigurableDoorActor::Tick(float DeltaSeconds)
+{
+    Super::Tick(DeltaSeconds);
+
+    if (!bIsAnimating || !DoorMotionRootComponent)
+    {
+        return;
+    }
+
+    CurrentAnimationTime += DeltaSeconds;
+
+    const float NormalizedAlpha = TransitionDuration <= KINDA_SMALL_NUMBER
+                                      ? 1.f
+                                      : FMath::Clamp(CurrentAnimationTime / TransitionDuration, 0.f, 1.f);
+
+    const float EvaluatedAlpha = EvaluateAnimationAlpha(NormalizedAlpha);
+    const FVector NewLocation = FMath::Lerp(AnimationStartLocation, AnimationTargetLocation, EvaluatedAlpha);
+    const FQuat NewRotation = FQuat::Slerp(AnimationStartRotation, AnimationTargetRotation, EvaluatedAlpha);
+
+    DoorMotionRootComponent->SetRelativeLocationAndRotation(NewLocation, NewRotation.Rotator());
+
+    if (NormalizedAlpha >= 1.f)
+    {
+        bIsAnimating = false;
+        bIsOpen = bAnimationTargetOpen;
+        ApplyDoorStateInstant(bIsOpen);
+    }
+}
+
+void AConfigurableDoorActor::ApplyDoorStateInstant(bool bOpenState)
+{
+    if (!DoorMotionRootComponent)
+    {
+        return;
+    }
+
+    const FTransform TargetTransform = bOpenState ? GetOpenedDoorTransform() : GetClosedDoorTransform();
+    DoorMotionRootComponent->SetRelativeLocationAndRotation(
+        TargetTransform.GetLocation(),
+        TargetTransform.GetRotation().Rotator());
+}
+
+void AConfigurableDoorActor::StartDoorAnimation(bool bOpenState)
+{
+    if (!DoorMotionRootComponent)
+    {
+        return;
+    }
+
+    if (TransitionDuration <= KINDA_SMALL_NUMBER)
+    {
+        bIsAnimating = false;
+        bIsOpen = bOpenState;
+        ApplyDoorStateInstant(bOpenState);
+        return;
+    }
+
+    const FTransform TargetTransform = bOpenState ? GetOpenedDoorTransform() : GetClosedDoorTransform();
+
+    bAnimationTargetOpen = bOpenState;
+    bIsAnimating = true;
+    CurrentAnimationTime = 0.f;
+    AnimationStartLocation = DoorMotionRootComponent->GetRelativeLocation();
+    AnimationStartRotation = DoorMotionRootComponent->GetRelativeRotation().Quaternion();
+    AnimationTargetLocation = TargetTransform.GetLocation();
+    AnimationTargetRotation = TargetTransform.GetRotation();
+}
+
+float AConfigurableDoorActor::EvaluateAnimationAlpha(float NormalizedAlpha) const
+{
+    const float ClampedAlpha = FMath::Clamp(NormalizedAlpha, 0.f, 1.f);
+
+    if (!TransitionCurve)
+    {
+        return ClampedAlpha;
+    }
+
+    return FMath::Clamp(TransitionCurve->GetFloatValue(ClampedAlpha), 0.f, 1.f);
+}
+
+FTransform AConfigurableDoorActor::GetClosedDoorTransform() const
+{
+    return FTransform(ClosedRelativeRotation, ClosedRelativeLocation);
+}
+
+FTransform AConfigurableDoorActor::GetOpenedDoorTransform() const
+{
+    return FTransform(OpenRelativeRotation, OpenRelativeLocation);
+}
+
+void AConfigurableDoorActor::OpenDoor()
+{
+    SetDoorOpenState(true, false);
+}
+
+void AConfigurableDoorActor::CloseDoor()
+{
+    SetDoorOpenState(false, false);
+}
+
+void AConfigurableDoorActor::ToggleDoor()
+{
+    const bool bNextOpenState = bIsAnimating ? !bAnimationTargetOpen : !bIsOpen;
+    SetDoorOpenState(bNextOpenState, false);
+}
+
+void AConfigurableDoorActor::SetDoorOpenState(bool bNewOpenState, bool bInstant)
+{
+    bAnimationTargetOpen = bNewOpenState;
+
+    if (bInstant)
+    {
+        bIsAnimating = false;
+        bIsOpen = bNewOpenState;
+        ApplyDoorStateInstant(bNewOpenState);
+        return;
+    }
+
+    StartDoorAnimation(bNewOpenState);
+}
+
+void AConfigurableDoorActor::CaptureCurrentAsClosedState()
+{
+    if (!DoorMotionRootComponent)
+    {
+        return;
+    }
+    #if WITH_EDITOR
+        Modify();
+    #endif
+    ClosedRelativeLocation = DoorMotionRootComponent->GetRelativeLocation();
+    ClosedRelativeRotation = DoorMotionRootComponent->GetRelativeRotation();
+
+    #if WITH_EDITOR
+        MarkPackageDirty();
+        PostEditChange();
+    #endif
+}
+
+void AConfigurableDoorActor::CaptureCurrentAsOpenState()
+{
+    if (!DoorMotionRootComponent)
+    {
+        return;
+    }
+
+    #if WITH_EDITOR
+        Modify();
+    #endif
+
+    OpenRelativeLocation = DoorMotionRootComponent->GetRelativeLocation();
+    OpenRelativeRotation = DoorMotionRootComponent->GetRelativeRotation();
+
+    #if WITH_EDITOR
+        MarkPackageDirty();
+        PostEditChange();
+    #endif
+}
+
+void AConfigurableDoorActor::PreviewClosedState()
+{
+    ApplyDoorStateInstant(false);
+}
+
+void AConfigurableDoorActor::PreviewOpenState()
+{
+    ApplyDoorStateInstant(true);
+}
---
+++ ConfigurableDoorActor.h
@@ -0,0 +1,155 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ConfigurableDoorActor.generated.h"
+
+class UCurveFloat;
+class USceneComponent;
+class UStaticMeshComponent;
+
+UCLASS(Blueprintable)
+class KONGBU_API AConfigurableDoorActor : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    AConfigurableDoorActor();
+
+    virtual void Tick(float DeltaSeconds) override;
+    virtual void OnConstruction(const FTransform& Transform) override;
+
+protected:
+    virtual void BeginPlay() override;
+
+    void ApplyDoorStateInstant(bool bOpenState);
+    void StartDoorAnimation(bool bOpenState);
+    float EvaluateAnimationAlpha(float NormalizedAlpha) const;
+    FTransform GetClosedDoorTransform() const;
+    FTransform GetOpenedDoorTransform() const;
+
+    // --- Components ---
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    TObjectPtr<USceneComponent> SceneRootComponent;
+
+    // 额外的门框、柜体、装饰网格建议挂到这个节点下面；它们不会跟着门一起动。
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    TObjectPtr<USceneComponent> StaticMeshesRootComponent;
+
+    // 只有这个节点会做开关过渡；门本体网格挂在这里
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    TObjectPtr<USceneComponent> DoorMotionRootComponent;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    TObjectPtr<UStaticMeshComponent> DoorMeshComponent;
+
+    // --- Door | Transform ---
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Door|Transform")
+    FVector ClosedRelativeLocation = FVector::ZeroVector;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Door|Transform")
+    FRotator ClosedRelativeRotation = FRotator::ZeroRotator;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Door|Transform")
+    FVector OpenRelativeLocation = FVector::ZeroVector;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Door|Transform")
+    FRotator OpenRelativeRotation = FRotator(0.f, 90.f, 0.f);
+
+    // --- Door | Animation ---
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Door|Animation", meta = (ClampMin = "0.0"))
+    float TransitionDuration = 0.5f;
+
+    // 可选曲线，输入时间范围建议为 0~1，输出也建议为 0~1。
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Door|Animation")
+    TObjectPtr<UCurveFloat> TransitionCurve = nullptr;
+
+    // --- Door | Setup ---
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Door|Setup")
+    bool bStartOpened = false;
+
+    // --- Door | State ---
+
+    UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = "Door|State")
+    bool bIsOpen = false;
+
+    UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = "Door|State")
+    bool bIsAnimating = false;
+
+    // --- Transient Properties (Runtime Only) ---
+
+    UPROPERTY(Transient)
+    bool bAnimationTargetOpen = false;
+
+    UPROPERTY(Transient)
+    float CurrentAnimationTime = 0.f;
+
+    UPROPERTY(Transient)
+    FVector AnimationStartLocation = FVector::ZeroVector;
+
+    UPROPERTY(Transient)
+    FQuat AnimationStartRotation = FQuat::Identity;
+
+    UPROPERTY(Transient)
+    FVector AnimationTargetLocation = FVector::ZeroVector;
+
+    UPROPERTY(Transient)
+    FQuat AnimationTargetRotation = FQuat::Identity;
+
+public:
+    // --- Public API Functions ---
+
+    UFUNCTION(BlueprintCallable, Category = "Door")
+    void OpenDoor();
+
+    UFUNCTION(BlueprintCallable, Category = "Door")
+    void CloseDoor();
+
+    UFUNCTION(BlueprintCallable, Category = "Door")
+    void ToggleDoor();
+
+    UFUNCTION(BlueprintCallable, Category = "Door")
+    void SetDoorOpenState(bool bNewOpenState, bool bInstant = false);
+
+    UFUNCTION(BlueprintPure, Category = "Door")
+    bool IsDoorOpen() const
+    {
+        return bIsOpen;
+    }
+
+    UFUNCTION(BlueprintPure, Category = "Door")
+    bool IsDoorAnimating() const
+    {
+        return bIsAnimating;
+    }
+
+    UFUNCTION(BlueprintPure, Category = "Door|Components")
+    UStaticMeshComponent* GetDoorMeshComponent() const
+    {
+        return DoorMeshComponent;
+    }
+
+    UFUNCTION(BlueprintPure, Category = "Door|Components")
+    USceneComponent* GetStaticMeshesRootComponent() const
+    {
+        return StaticMeshesRootComponent;
+    }
+
+    // --- Editor Utilities ---
+
+    UFUNCTION(CallInEditor, BlueprintCallable, Category = "Door|Editor")
+    void CaptureCurrentAsClosedState();
+
+    UFUNCTION(CallInEditor, BlueprintCallable, Category = "Door|Editor")
+    void CaptureCurrentAsOpenState();
+
+    UFUNCTION(CallInEditor, BlueprintCallable, Category = "Door|Editor")
+    void PreviewClosedState();
+
+    UFUNCTION(CallInEditor, BlueprintCallable, Category = "Door|Editor")
+    void PreviewOpenState();
+};
